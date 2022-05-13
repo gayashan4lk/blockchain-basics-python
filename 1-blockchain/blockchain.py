da4adaa4cd5c0e27b49b38f29d92dccd43a9e9af
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-05-13 17:01:53 +0530

    implement blockchain class

--- 1-blockchain/blockchain.py
+++ 1-blockchain/blockchain.py
@@ -53,3 +53,39 @@ class Blockchain:
                 return False
             previous_block = block
             block_index += 1
+        return True
+    
+# creating the web app
+app = Flask(__name__)
+# This Line added because of error : 'Request' object has no attribute 'is_xhr'
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+# creating the blockchain
+blockchain = Blockchain()
+
+# mining a new block
+@app.route('/mine_block', methods = ['GET'])
+def mine_block():
+    previous_block = blockchain.get_previous_block()
+    previous_proof = previous_block['proof']
+    proof = blockchain.proof_of_work(previous_proof)
+    previous_hash = blockchain.hash(previous_block)
+    block = blockchain.create_block(proof, previous_hash)
+    response = {
+        'message': 'Congratulations, You just mined a block!',
+        'index': block['index'],
+        'timestamp': block['timestamp'],
+        'proof': block['proof'],
+        'previous_hash': block['previous_hash']
+        }
+    return jsonify(response), 200
+
+# getting the full blockchain
+@app.route('/get_chain', methods = ['GET'])
+def get_chain():
+    response = {'chain': blockchain.chain,
+                'length': len(blockchain.chain)}
+    return jsonify(response), 200
+
+# running the app
+app.run(host = '0.0.0.0', port = 5000)
